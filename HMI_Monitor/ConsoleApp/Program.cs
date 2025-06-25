5eef227c123493d36702cc66ef31ecb191f4e375
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2025-06-25 06:31:29 -0300

    Added performance

--- HMI_Monitor/ConsoleApp/Program.cs
+++ HMI_Monitor/ConsoleApp/Program.cs
@@ -2,17 +2,17 @@
 using Renci.SshNet;
 
 Console.WriteLine("HMI Monitor - Console Version!");
-Console.WriteLine("------------------------------");
-Console.WriteLine("\n\n\n");
+Console.WriteLine("------------------------------\n");
 
 ConnectionInfo conn;
 SshClient sshClient;
 
 string ipAddress = "191.252.222.248";
-string keyFile = string.Empty;
 string keyFilePath = "C:\\Users\\redto\\OneDrive\\Documentos\\repositories\\Engineering\\pc\\hmi_monitor\\HMI_Monitor\\ConsoleApp\\bin\\Debug\\net8.0\\id_ed25519";
 
-
+string cpuConsumptionResult = string.Empty;
+string ramConsumptionResult = string.Empty;
+string hdConsumptionResult = string.Empty;
 
 try
 {
@@ -20,7 +20,7 @@ try
     using StreamReader reader = new(keyFilePath);
 
     // Read the stream as a string.
-    keyFile = reader.ReadToEnd();
+    string text = reader.ReadToEnd();
 }
 catch (IOException e)
 {
@@ -36,7 +36,31 @@ try
                 });
 
     Console.WriteLine("HMI Monitor - Connection OK");
+
+    using (sshClient = new SshClient(conn))
+    {
+        sshClient.Connect();
+        
+        var result = sshClient.RunCommand("echo \"CPU $(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\\([0-9.]*\\)%* id.*/\\1/' | awk '{print 100 - $1}')% RAM $(free -m | awk '/Mem:/ { printf(\"%3.1f%%\", $3/$2*100) }') HDD $(df -h / | awk '/\\// {print $(NF-1)}')\"");
+        cpuConsumptionResult = sshClient.RunCommand("echo \"$(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\\([0-9.]*\\)%* id.*/\\1/' | awk '{print 100 - $1}')%\"").Result;
+        ramConsumptionResult = sshClient.RunCommand("echo \"RAM $(free -m | awk '/Mem:/ { printf(\"%3.1f%%\", $3/$2*100) }')\"").Result;
+        hdConsumptionResult = sshClient.RunCommand("echo \"HDD $(df -h / | awk '/\\// {print $(NF-1)}')\"").Result;
+
+        while (result.Result == string.Empty)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine(" - Waiting");
+        }
+
+        Console.WriteLine("Command Output:");
+        Console.WriteLine(result.Result);
+        Console.WriteLine(cpuConsumptionResult);
+        Console.WriteLine(ramConsumptionResult);
+        Console.WriteLine(hdConsumptionResult);
+
+    }
 }
 catch(Exception e) {
     Console.WriteLine($"HMI Monitor - Error trying to connect: {e}",e);
 }
+
